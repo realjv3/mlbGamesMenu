73f62af6d6cc0b09a7f696cb92e0e3161c4c9feb
John Verity <jverity@fatguymedia.com>
2020-08-30 12:48:22 -0400

    Changes to https mlb endpoint

--- games.js
+++ games.js
@@ -6,7 +6,7 @@ export const getGames = () => {
 		month = `${now.getMonth() + 1}`.padStart(2, '0'),
 		day = `${now.getDate()}`.padStart(2, '0');
 		return fetch(
-			`http://statsapi.mlb.com/api/v1/schedule?hydrate=game(content(editorial(recap))),decisions&date=${year}-${month}-${day}&sportId=1`
+			`https://statsapi.mlb.com/api/v1/schedule?hydrate=game(content(editorial(recap))),decisions&date=${year}-${month}-${day}&sportId=1`
 		)
 			.then(resp => resp.json());
 }
